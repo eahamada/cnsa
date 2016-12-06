5dbd7d304a24577bab12e421ff75f6285edd77e4
eahamada <elarif.ahamada@gmail.com>
2016-12-06 13:32:32 +0100

    Update 03.install-jboss.sh

--- 03.install-jboss.sh
+++ 03.install-jboss.sh
@@ -59,7 +59,9 @@ keytool -importcert \
 sed -i -e '29 i\<system-properties>\n<property name="javax.net.ssl.trustStore" value="'$KEYSTORE_PATH'/ldapTrusStore"/>\n <property name="javax.net.ssl.trustStorePassword" value="'$KEYSTORE_PASSWORD'"/>\n</system-properties>' -- $JBOSS_HOME/standalone/configuration/standalone.xml
 sed -i -e '284 i\<inet-address value="${jboss.bind.address.management:0.0.0.0}"/>' -e '287 i\<inet-address value="${jboss.bind.address:0.0.0.0}"/>'  -e '284d;287d' -- $JBOSS_HOME/standalone/configuration/standalone.xml
 sed -i -e '200 i\<subsystem xmlns="urn:jboss:domain:naming:1.1">\n<bindings>\n<simple name="java:global/sepannuaire.ws.config.path" value="'$CONF_PATH_WS'" type="java.lang.String"/>\n<simple name="java:global/sepannuaire.web.config.path" value="'$CONF_PATH_WEB'" type="java.lang.String"/>\n</bindings>\n</subsystem>\n' -e '200d' -- $JBOSS_HOME/standalone/configuration/standalone.xml
+chown -Rf $JBOSS_USER:$JBOSS_GROUP $JBOSS_HOME
 service jboss start
+ 
 
 ## 5.2.5	CONFIGURATION DE L’APPLICATION
 # mkdir -p $CONF_PATH_WS && mv ldap.properties mail.properties web-services.properties rest-services.properties traces.properties web-services-finess.properties applications.properties $CONF_PATH_WS
