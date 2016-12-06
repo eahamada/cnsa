20e0c6f88ea95650eff67d995a528083895be962
eahamada <elarif.ahamada@gmail.com>
2016-12-06 11:26:14 +0100

    Update 03.install-jboss.sh

--- 03.install-jboss.sh
+++ 03.install-jboss.sh
@@ -11,9 +11,9 @@ export KEYSTORE_PATH=${KEYSTORE_PATH:=/root/.keystore}
 export KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD:=passw0rd}
 export WORKDIR=${WORKDIR:=`mktemp -d`}
 rm -fr $JBOSS_HOME $WORKDIR/* $KEYSTORE_PATH
-ps -o pid= -u jboss | xargs kill -1
-userdel -fr $JBOSS_USER
+$(ps -o pid= -u $JBOSS_USER | xargs kill -1 2>/dev/null) || echo 'no process to kill'userdel -fr $JBOSS_USER
 groupdel $JBOSS_GROUP
+userdel -fr $JBOSS_USER
 cd $WORKDIR
 
 wget http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.tar.gz
